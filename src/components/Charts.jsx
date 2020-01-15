a315bbb8e1b0413c0274293ce9ef5e13a138e389
Jasmine Terry <jasmine.terry91@gmail.com>
2020-01-15 17:00:08 -0500

    STRETCH(adding data from api)

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -10,9 +10,13 @@ const Charts = ({ coinData }) => {
           <h4 className="coin__symbol">{coin.symbol}</h4>
           <div className="coin__logo">
             <img src={coin.image} height="40" alt={coin.name} />
+      <h4>Price Change(24hrs): {coin.price_change_24h}</h4> 
           </div>
-          <Chart sparklineData={coin.sparkline_in_7d.price} />
+          <Chart sparklineData={coin.sparkline_in_7d.price}/>
+         <p>COIN HIGH(24hrs): {coin.high_24h}</p>
+          <span><h3>[ Market Cap: { coin.market_cap} ]</h3></span>
         </div>
+        
       ))}
     </div>
   );
